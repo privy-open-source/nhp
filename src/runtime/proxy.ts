9126d55d9127547286d45f99777e6899404445d3
Ade Novid <adenvt@gmail.com>
2023-06-08 11:02:33 +0700

    fix: fix path rewrite when value is a function

--- src/runtime/proxy.ts
+++ src/runtime/proxy.ts
@@ -3,19 +3,22 @@ import { fromNodeMiddleware } from 'h3'
 import { createProxyMiddleware } from 'http-proxy-middleware'
 import console from 'consola'
 import config from '~/server.config'
+import type { ApiServer } from '../core/'
 
 const app = connect()
 
-for (const server of config) {
+for (const server of (config as ApiServer[])) {
   if (server.targetUrl) {
     const proxy = createProxyMiddleware(server.baseUrl, {
       ...server,
       target      : server.targetUrl,
       changeOrigin: true,
-      pathRewrite : {
-        ...server.pathRewrite,
-        [`^${server.baseUrl}`]: '/',
-      },
+      pathRewrite : typeof server.pathRewrite === 'function'
+        ? server.pathRewrite
+        : {
+          [`^${server.baseUrl}`]: '/',
+          ...server.pathRewrite,
+        },
     })
 
     app.use(proxy as connect.NextHandleFunction)
