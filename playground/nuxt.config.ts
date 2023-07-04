bd0bfcb08cde0bb19ff831f5a7da03571845281a
Pringgo Radianto <pringgo.radianto@privyid.tech>
2023-07-04 14:26:07 +0700

    chore(ts): enable strict typescript rules

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   typescript: {
     tsConfig: {
       compilerOptions: {
-        strict          : false,
+        strict          : true,
         strictNullChecks: true,
       },
     },
