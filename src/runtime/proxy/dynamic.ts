eed800c821812640790c608ad400d36ab03e0872
Ade Novid <adenvt@gmail.com>
2023-12-08 17:20:41 +0700

    fix: proxy not working when changing baseURL

--- src/runtime/proxy/dynamic.ts
+++ src/runtime/proxy/dynamic.ts
@@ -1,8 +1,13 @@
 import type connect from 'connect'
 import type { ApiServer } from '../../core'
 import type { IncomingMessage } from 'node:http'
-import { getQuery, parseURL } from 'ufo'
+import {
+  getQuery,
+  joinURL,
+  parseURL,
+} from 'ufo'
 import { createProxyMiddleware } from 'http-proxy-middleware'
+import { useRuntimeConfig } from '#imports'
 
 function validURL (url: string) {
   const pattern = new RegExp('^(https?:\\/\\/)' // protocol
@@ -15,8 +20,11 @@ function validURL (url: string) {
   return !!pattern.test(url)
 }
 
-export default function addDynamicProxy (app: connect.Server, config: ApiServer) {
-  app.use(config.baseUrl, (req, res, next) => {
+export default function addDynamicProxy (app: connect.Server, proxy: ApiServer) {
+  const config  = useRuntimeConfig()
+  const baseUrl = joinURL(config.app.baseURL, proxy.baseUrl)
+
+  app.use(baseUrl, (req, res, next) => {
     const query = getQuery(req.url as string)
 
     if (!query.url || !validURL(query.url as string)) {
@@ -31,9 +39,9 @@ export default function addDynamicProxy (app: connect.Server, config: ApiServer)
     }
 
     const url       = parseURL(query.url as string)
-    const whitelist = Array.isArray(config.allowFrom)
-      ? config.allowFrom
-      : (config.allowFrom ?? '').split(';')
+    const whitelist = Array.isArray(proxy.allowFrom)
+      ? proxy.allowFrom
+      : (proxy.allowFrom ?? '').split(';')
 
     if (!url.host || !whitelist.includes(url.host)) {
       res.writeHead(403)
@@ -49,9 +57,9 @@ export default function addDynamicProxy (app: connect.Server, config: ApiServer)
     next()
   })
 
-  app.use(createProxyMiddleware(config.baseUrl, {
-    ...config,
-    target      : config.targetUrl ?? 'http://proxy.com',
+  app.use(createProxyMiddleware(baseUrl, {
+    ...proxy,
+    target      : proxy.targetUrl ?? 'http://proxy.com',
     changeOrigin: true,
     router (request) {
       const query = getQuery((request as IncomingMessage).url ?? '')
@@ -66,22 +74,22 @@ export default function addDynamicProxy (app: connect.Server, config: ApiServer)
       return `${url.pathname}${url.search}`
     },
     onProxyRes (proxyResponse, request, response) {
-      if (config.downloadHeader) {
+      if (proxy.downloadHeader) {
         const query = getQuery((request as IncomingMessage).url ?? '')
 
         if (query.download !== 'false') {
           const name     = query.name?.toString() ?? 'download'
           const type     = query.type?.toString() ?? 'attachment'
-          const filename = config.downloadExt && !name.endsWith(config.downloadExt)
-            ? `${name}${config.downloadExt}`
+          const filename = proxy.downloadExt && !name.endsWith(proxy.downloadExt)
+            ? `${name}${proxy.downloadExt}`
             : name
 
           proxyResponse.headers['content-disposition'] = `${type}; filename="${encodeURIComponent(filename)}"`
         }
       }
 
-      if (config.onProxyRes)
-        config.onProxyRes(proxyResponse, request, response)
+      if (proxy.onProxyRes)
+        proxy.onProxyRes(proxyResponse, request, response)
     },
   }) as connect.NextHandleFunction)
 }
